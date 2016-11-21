fdc5fe0a05073d709dc6c4b09e08b3d3c3ddb88f
Craig <blitzkriegx3@gmail.com>
2016-11-20 23:31:22 -0500

    added filter todos with query params

---
+++ variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: "Craig",
+// 	age: 32
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: "Craig",
+// 	// 	age: 22
+// 	// };
+
+// 	obj.age = 22;
+// };
+
+// updatePerson(person);
+// console.log(person);
+var grades = [50, 70];
+function addGrade(gradesArr) {
+	gradesArr.push(55);
+	debugger;
+
+	//gradesArr = [12, 15, 17];
+
+}
+
+addGrade(grades);
+console.log(grades);
