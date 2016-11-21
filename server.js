04cc6d97e0eab7ac34d8a9fa5748e3522471520f
Kalkrin <blitzkriegx3@gmail.com>
2016-11-21 17:09:05 -0500

    final static todos

--- server.js
+++ server.js
@@ -19,10 +19,17 @@ app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
 
+	//Filter todos by either true or false
 	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	res.json(filteredTodos);
