7ab1b2cd22ecc9cc070d4ed3c8fb776deee78584
Kalkrin <blitzkriegx3@gmail.com>
2016-12-02 17:50:10 -0500

    add auth middleware and findByTokenClass method

--- models/user.js
+++ models/user.js
@@ -37,6 +37,7 @@ module.exports = function(sequelize, DataTypes) {
 	}, {
 		hooks: {
 			beforeValidate: function(user, options) {
+				// user.email
 				if (typeof user.email === 'string') {
 					user.email = user.email.toLowerCase();
 				}
@@ -48,6 +49,7 @@ module.exports = function(sequelize, DataTypes) {
 					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
 						return reject();
 					}
+
 					user.findOne({
 						where: {
 							email: body.email
@@ -62,6 +64,27 @@ module.exports = function(sequelize, DataTypes) {
 						reject();
 					});
 				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function (user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function (e) {
+							reject();
+						});
+					} catch (e) {
+						reject();
+					}
+				});
 			}
 		},
 		instanceMethods: {
@@ -70,23 +93,28 @@ module.exports = function(sequelize, DataTypes) {
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			},
 			generateToken: function(type) {
-				if(!_.isString(type)) {
+				if (!_.isString(type)) {
 					return undefined;
 				}
 
-				try{
-					var stringData = JSON.stringify({id: this.get('id'), type: type})
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
 					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
 					var token = jwt.sign({
 						token: encryptedData
 					}, 'qwerty098');
+
 					return token;
-				} catch(e) {
+				} catch (e) {
 					console.error(e);
 					return undefined;
 				}
 			}
 		}
 	});
+
 	return user;
 };
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcryptjs');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
-var todoNextId = 1
+var todoNextId = 1;
 
 app.use(bodyParser.json());
 
@@ -16,7 +17,7 @@ app.get('/', function(req, res) {
 });
 
 //Get request to display all todo items.
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {};
 
@@ -47,7 +48,7 @@ app.get('/todos', function(req, res) {
 });
 
 //Get request to show todo item by id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.findById(todoId).then(function(todo) {
@@ -64,7 +65,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 //Adding a new todo
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
@@ -74,7 +75,7 @@ app.post('/todos', function(req, res) {
 	});
 });
 
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -94,7 +95,7 @@ app.delete('/todos/:id', function(req, res) {
 	});
 });
 
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
